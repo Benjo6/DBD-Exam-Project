52960f6bad779eae8028f5983ec2b0c66a588628
Daniel Grønbjerg <danielgronbjerg@gmail.com>
2022-05-27 10:53:51 +0200

    Add services to compose

--- DBD-Exam-Project/PrescriptionService/Program.cs
+++ DBD-Exam-Project/PrescriptionService/Program.cs
@@ -27,14 +27,13 @@ builder.Services.AddScoped<IAsyncRepository<Pharmacy>, PharmacyRepository>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
     app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });
 
-app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
