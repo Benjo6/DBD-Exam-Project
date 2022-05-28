203d3bc910124b697ae28f04226c4475c6c78bd8
Benjo6 <58556176+Benjo6@users.noreply.github.com>
2022-05-28 15:34:26 +0200

    Revert "Update Program.cs"
    This reverts commit c8dfdab6d12cc07dd1514c3eb03c202f2db65d4b.

--- DBD-Exam-Project/Neo4J/Program.cs
+++ DBD-Exam-Project/Neo4J/Program.cs
@@ -13,7 +13,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container
 
 builder.Services.AddControllers();
-
+builder.Services.Configure<JsonOptions>(options =>
+{
+    options.JsonSerializerOptions.Converters.Add(new DateOnlyJsonConverter());
+});
 
 
 builder.Services.AddEndpointsApiExplorer();
