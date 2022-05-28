dcd5841cb95bd52deabe5f7daeeaa8c6ea8fa7f4
Daniel Grønbjerg <danielgronbjerg@gmail.com>
2022-05-28 23:23:13 +0200

    Add newtonsoft -> cs-proj

--- DBD-Exam-Project/CronJobService/Services/ConsultationCreationService.cs
+++ DBD-Exam-Project/CronJobService/Services/ConsultationCreationService.cs
@@ -5,10 +5,7 @@ using Newtonsoft.Json;
 using RestSharp;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace CronJobService.Services
 {
