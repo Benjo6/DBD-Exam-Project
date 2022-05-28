b74d8e82652a0f286cd766b6c72b34be631ed5a1
Benjo6 <58556176+Benjo6@users.noreply.github.com>
2022-05-28 16:14:49 +0200

    Neo4j
    fff

--- DBD-Exam-Project/Neo4JDataSupplier/Program.cs
+++ DBD-Exam-Project/Neo4JDataSupplier/Program.cs
@@ -1,12 +1,10 @@
 ﻿using System;
 using System.Configuration;
 using System.Threading.Tasks;
-using ConsultationService;
-using ConsultationService.Services;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Neo4jClient;
-using PrescriptionService.DAP;
 
 namespace Neo4JDataSupplier
 {
@@ -17,12 +15,9 @@ namespace Neo4JDataSupplier
             var client = new BoltGraphClient(new Uri("bolt://localhost:7687"), "neo4j", "2wsxcde3");
             await client.ConnectAsync();
             var builder = Host.CreateDefaultBuilder()
-                .ConfigureServices((hostContext,services) =>
+                .ConfigureServices((hostContext, services) =>
                 {
-                    services.AddScoped<IConsultationService, MongoConsultationService>();
-                    var connectionString = "Host=localhost;Port=15432;Database=prescription_db;Include Error Detail=true;Username=prescription_user;Password=prescription_pw";
-                    string customConnString = "Host=localhost;Port=5432;Database=prescription_db;Include Error Detail=true;Username={user};Password={pass}";
-                    services.AddSingleton<IPrescriptionRepo>(new DapperPrescriptionRepo(connectionString, customConnString));
+
                     services.AddTransient<Neo4jClient>();
                     services.AddSingleton<IGraphClient>(client);
                 }).UseConsoleLifetime();
@@ -38,7 +33,7 @@ namespace Neo4JDataSupplier
                     var myService = services.GetRequiredService<Neo4jClient>();
                     var result = await myService.Run();
 
-                    Console.WriteLine($"{string.Join(", ",result)}");
+                    Console.WriteLine($"{string.Join(", ", result)}");
                 }
                 catch (Exception ex)
                 {
@@ -46,7 +41,7 @@ namespace Neo4JDataSupplier
                 }
             }
 
-            
+
         }
 
 
