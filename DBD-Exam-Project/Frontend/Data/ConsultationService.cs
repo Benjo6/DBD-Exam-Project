b1ca288d45a3776122ee398fc55be1f5c23fb77b
Svensmark <emil.svensmark@gmail.com>
2022-05-29 23:38:58 +0200

    Got antblazor table to work.

---
+++ DBD-Exam-Project/Frontend/Data/ConsultationService.cs
@@ -0,0 +1,22 @@
+﻿using lib.DTO;
+
+namespace Frontend.Data;
+
+public class ConsultationService : IConsultationService
+{
+    private HttpClient _client;
+
+    public ConsultationService(IHttpClientFactory factory)
+    {
+        _client = factory.CreateClient("ConsultationClient");
+    }
+
+    public async Task<List<ConsultationDto>> GetConsultationsAsync()
+        => await _client.GetFromJsonAsync<List<ConsultationDto>>("Consultation")
+            ?? new List<ConsultationDto>();
+}
+
+public interface IConsultationService
+{
+    Task<List<ConsultationDto>> GetConsultationsAsync();
+}
