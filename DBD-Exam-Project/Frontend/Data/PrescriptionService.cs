6d2081ca050bfcc7d81bb893e49a975af12b1f9a
Sofus Hilfling Nielsen <sofushn@hotmail.dk>
2022-05-30 23:11:04 +0200

    use dynamic paging in user view

--- DBD-Exam-Project/Frontend/Data/PrescriptionService.cs
+++ DBD-Exam-Project/Frontend/Data/PrescriptionService.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json;
+﻿using System.Text;
+using System.Text.Json;
 using lib.DTO;
 
 namespace Frontend.Data;
@@ -16,8 +17,8 @@ public class PeopleService : IPeopleService
         => await _client.GetFromJsonAsync<IEnumerable<PersonDto>>(GetPeopleBaseUrl(null))
             ?? Enumerable.Empty<PersonDto>();
 
-    public async Task<IEnumerable<PersonDto>> GetPeopleAsync(PersonType type)
-        => await _client.GetFromJsonAsync<IEnumerable<PersonDto>>(GetPeopleBaseUrl(type)) 
+    public async Task<IEnumerable<PersonDto>> GetPeopleAsync(PersonType type, int pageCount = 0, int pageSize = 0)
+        => await _client.GetFromJsonAsync<IEnumerable<PersonDto>>(GetPeopleBaseUrl(type) + GetPageParameter(pageCount, pageSize))
             ?? Enumerable.Empty<PersonDto>();
 
     public async Task<PersonDto> GetPersonDetails(int id, PersonType type)
@@ -36,12 +37,29 @@ public class PeopleService : IPeopleService
             PersonType.Pharmaceut => "Persons/pharmaceuts",
             _ => "Persons"
         };
+
+    private string GetPageParameter(int pageCount, int pageSize)
+    {
+        StringBuilder sb = new("?");
+
+        if (pageCount > 0)
+            sb.Append($"Number={pageCount}");
+        if (pageSize > 0)
+        {
+            if (pageCount > 0)
+                sb.Append('&');
+            
+            sb.Append($"Size={pageSize}");
+        }
+
+        return sb.ToString();
+    }
 }
 
 public interface IPeopleService
 {
     Task<IEnumerable<PersonDto>> GetPeopleAsync();
-    Task<IEnumerable<PersonDto>> GetPeopleAsync(PersonType type);
+    Task<IEnumerable<PersonDto>> GetPeopleAsync(PersonType type, int pageCount = 0, int pageSize = 0);
     Task<PersonDto> GetPersonDetails(int id, PersonType type);
     Task<PersonDto> GetPatientDetails(string cprNumber);
 }
