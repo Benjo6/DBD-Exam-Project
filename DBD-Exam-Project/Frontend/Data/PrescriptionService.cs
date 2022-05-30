f91972c479c2a6a2fe882f84e2f174f936b41503
Sofus Hilfling Nielsen <sofushn@hotmail.dk>
2022-05-31 01:48:03 +0200

    Added unimplemented function to PrescriptionService

--- DBD-Exam-Project/Frontend/Data/PrescriptionService.cs
+++ DBD-Exam-Project/Frontend/Data/PrescriptionService.cs
@@ -1,4 +1,6 @@
-﻿namespace Frontend.Data;
+﻿using lib.DTO;
+
+namespace Frontend.Data;
 
 public class PrescriptionService: IPrescriptionService
 {
@@ -12,9 +14,40 @@ public class PrescriptionService: IPrescriptionService
     public async Task<IEnumerable<string>> GetMedicine()
         => await _client.GetFromJsonAsync<IEnumerable<string>>("Medicines")
             ?? Enumerable.Empty<string>();
+
+    public Task<bool> FulfillPrescription(long id)
+    {
+        return Task.FromResult(true);
+    }
+
+    public Task<IEnumerable<PrescriptionDto>> GetAllPrescriptions(int pageCount = 0, int pageSize = 0)
+    {
+        return Task.FromResult(Enumerable.Empty<PrescriptionDto>());
+    }
+
+    public Task<IEnumerable<PrescriptionDto>> GetPrescriptionsForDoctor(int doctorId, int pageCount = 0, int pageSize = 0)
+    {
+        return Task.FromResult(Enumerable.Empty<PrescriptionDto>());
+    }
+
+    public Task<IEnumerable<PrescriptionDto>> GetPrescriptionsForPatient(string cprNumber, int pageCount = 0, int pageSize = 0)
+    {
+        return Task.FromResult(Enumerable.Empty<PrescriptionDto>());
+    }
+
+    public Task<PrescriptionDto> CreatePrescription(PrescriptionDto newPrescription)
+    {
+        return Task.FromResult(new PrescriptionDto());
+    }
 }
 
 public interface IPrescriptionService
 {
     Task<IEnumerable<string>> GetMedicine();
+    Task<bool> FulfillPrescription(long id);
+    Task<IEnumerable<PrescriptionDto>> GetAllPrescriptions(int pageCount = 0, int pageSize = 0);
+    Task<IEnumerable<PrescriptionDto>> GetPrescriptionsForDoctor(int doctorId, int pageCount = 0, int pageSize = 0);
+    Task<IEnumerable<PrescriptionDto>> GetPrescriptionsForPatient(string cprNumber, int pageCount = 0, int pageSize = 0);
+    Task<PrescriptionDto> CreatePrescription(PrescriptionDto newPrescription);
+    
 }
