a23712680af0a08dfb4cc9e518d78a7b45697c94
Benjo6 <58556176+Benjo6@users.noreply.github.com>
2022-05-30 10:53:51 +0200

    Small
    ff

---
+++ DBD-Exam-Project/Neo4jDataAnalyzer/Program.cs
@@ -0,0 +1,35 @@
+using Neo4jClient;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+var client = new BoltGraphClient(new Uri("bolt://localhost:7687"), "neo4j", "2wsxcde3");
+await client.ConnectAsync();
+builder.Services.AddSingleton<IGraphClient>(client);
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
