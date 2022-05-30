ee289d5f49ce19d752c9847d13c26f39ccf814e5
Sofus Hilfling Nielsen <sofushn@hotmail.dk>
2022-05-31 01:35:53 +0200

    Added page that shows medicine

---
+++ DBD-Exam-Project/Frontend/Data/PrescriptionService.cs
@@ -0,0 +1,20 @@
+﻿namespace Frontend.Data;
+
+public class PrescriptionService: IPrescriptionService
+{
+    private readonly HttpClient _client;
+
+    public PrescriptionService(IHttpClientFactory factory)
+    {
+        _client = factory.CreateClient("PrescriptionClient");
+    }
+
+    public async Task<IEnumerable<string>> GetMedicine()
+        => await _client.GetFromJsonAsync<IEnumerable<string>>("Medicines")
+            ?? Enumerable.Empty<string>();
+}
+
+public interface IPrescriptionService
+{
+    Task<IEnumerable<string>> GetMedicine();
+}
