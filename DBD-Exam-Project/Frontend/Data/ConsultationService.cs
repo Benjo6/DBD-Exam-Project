471be83c3933f20d2c486c509a36b039909f5359
Svensmark <emil.svensmark@gmail.com>
2022-05-30 21:40:34 +0200

    Added functionality to ConsultationService booking request.

--- DBD-Exam-Project/Frontend/Data/ConsultationService.cs
+++ DBD-Exam-Project/Frontend/Data/ConsultationService.cs
@@ -11,12 +11,20 @@ public class ConsultationService : IConsultationService
         _client = factory.CreateClient("ConsultationClient");
     }
 
+    public async Task BookAvailableConsultationAsync(ConsultationBookingRequestDto dto)
+    {
+        await _client.PutAsJsonAsync("Consultation/booking", dto);
+    }
+
     public async Task<List<ConsultationDto>> GetAvailableConsultationsAsync(double longitude, double latitude, int distance)
         => await _client.GetFromJsonAsync<List<ConsultationDto>>($"Consultation/booking/{longitude}/{latitude}/{distance}")
             ?? new List<ConsultationDto>();
+
 }
 
 public interface IConsultationService
 {
     Task<List<ConsultationDto>> GetAvailableConsultationsAsync(double longitude, double latitude, int distance);
+
+    Task BookAvailableConsultationAsync(ConsultationBookingRequestDto dto);
 }
