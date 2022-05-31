53e5179cbd6e7ae2a557c3b7b8336015ab52e84a
Daniel Grønbjerg <danielgronbjerg@gmail.com>
2022-05-31 15:47:04 +0200

    Add missing repl servers in kill-dev

--- kill-dev.bat
+++ kill-dev.bat
@@ -6,5 +6,7 @@ docker volume rm infrastructure_postgres_data
 docker volume rm infrastructure_mongo_router_data
 docker volume rm infrastructure_mongo_s1r1_data
 docker volume rm infrastructure_mongo_s1r2_data
+docker volume rm infrastructure_mongo_s1r3_data
 docker volume rm infrastructure_mongo_s2r1_data
 docker volume rm infrastructure_mongo_s2r2_data
+docker volume rm infrastructure_mongo_s2r3_data
